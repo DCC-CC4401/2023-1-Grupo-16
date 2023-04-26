b2c800223a1f79b8fe9ae432669664e82024ca0d
LeonelEspinoza <leonel.espinoza@ug.uchile.cl>
2023-04-26 19:03:17 -0400

    Feat database

--- rateyourseat/app_inicial/models.py
+++ rateyourseat/app_inicial/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Reviews(models.Model):
+        content=models.CharField(max_length=500)
+        sit_sector=models.CharField(max_length=15)
+        stars=models.IntegerField()
+        up_votes=models.IntegerField()
+        down_votes=models.IntegerField()
+        #author=models.Users()
+        date=models.DateTimeField()
+        #fotos 
+
+class Coments(models.Model):
+        content=models.CharField(max_length=250)
+        author=models.Object()
+        #father_review=models.Reviews()
+        date=models.DateTimeField()
+        
