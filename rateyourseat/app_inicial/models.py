06f67b5d4fec086ec6ce89ca786dc16b8586a96e
Gonzalo <gonzalo.cisternas@ug.uchile.cl>
2023-04-28 17:22:29 -0400

    feat: added classes for the database model

--- rateyourseat/app_inicial/models.py
+++ rateyourseat/app_inicial/models.py
@@ -1,19 +1,45 @@
 from django.db import models
 
 # Create your models here.
-class Reviews(models.Model):
-        content=models.CharField(max_length=500)
-        sit_sector=models.CharField(max_length=15)
-        stars=models.IntegerField()
-        up_votes=models.IntegerField()
-        down_votes=models.IntegerField()
-        #author=models.Users()
-        date=models.DateTimeField()
-        #fotos 
 
-class Coments(models.Model):
-        content=models.CharField(max_length=250)
-        author=models.Object()
-        #father_review=models.Reviews()
-        date=models.DateTimeField()
+#User (provisional due to how the password will be implemented)
+class User(models.Model):
+        name = models.CharField(max_length=20)
+        email = models.EmailField()
+        password = models.CharField(max_length=50)
+
+class Review(models.Model):
+        user = models.ForeignKey('User', on_delete=models.CASCADE)
+        concert = models.ForeignKey('Concert', on_delete=models.CASCADE)
+        content = models.TextField()                #models.CharField(max_length=500)
+        #foto
+        sit_sector = models.CharField(max_length=15)
+        stars = models.PositiveSmallIntegerField()          #models.IntegerField()
+        up_votes = models.PositiveSmallIntegerField()
+        down_votes = models.PositiveSmallIntegerField()
+        date = models.DateTimeField()
+
+class Comment(models.Model):
+        user = models.ForeignKey('User', on_delete=models.CASCADE)
+        content = models.TextField()                #models.CharField(max_length=500)
+        review = models.ForeignKey('Review', on_delete=models.CASCADE)
+        date = models.DateTimeField()
         
+
+class Concert(models.Model):
+        title = models.CharField(max_length=100)
+        artist = models.CharField(max_length=100)
+        date = models.DateField()
+        location = models.ForeignKey('Location', on_delete=models.CASCADE)
+
+class Location(models.Model):
+        name = models.CharField(max_length=100)
+        address = models.CharField(max_length=200)
+        city = models.CharField(max_length=100)
+        country = models.CharField(max_length=100)
+
+class Vote(models.Model):
+        user = models.ForeignKey('User', on_delete=models.CASCADE)
+        comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
+        is_positive = models.BooleanField()
+
