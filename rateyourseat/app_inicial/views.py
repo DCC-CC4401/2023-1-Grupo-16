84ea8bb1f11949ec8c2eb5346b511c3c59f51f4b
salocinc <nicolascalbucuravillalobos@gmail.com>
2023-05-11 18:25:06 -0400

    fix: modify an import XD

--- rateyourseat/app_inicial/views.py
+++ rateyourseat/app_inicial/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from django.template import Template, Context
-from django.template import loader
+from django.template.loader import get_template
 from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    home=loader.get_template('home.html')
+    home=get_template('home.html')
     home=home.render()
     return HttpResponse(home)
 
 def log_in(request):
-    log_in=loader.get_template('logIn.html')
+    log_in=get_template('logIn.html')
     log_in=log_in.render()
     return HttpResponse(log_in)
 
 def sign_up(request):
-    sign_up=loader.get_template('signUp.html')
+    sign_up=get_template('signUp.html')
     sign_up=sign_up.render()
     return HttpResponse(sign_up)
