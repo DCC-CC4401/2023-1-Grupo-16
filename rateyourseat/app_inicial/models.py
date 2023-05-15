d9ab8a29f2ba611b295521567673a1764640fe6b
JavieraJimenezR <javiera.jimenez.rojas.99@gmail.com>
2023-05-14 23:48:34 -0400

    add: models methods documentation

--- rateyourseat/app_inicial/models.py
+++ rateyourseat/app_inicial/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
-#User (provisional due to how the password will be implemented)
+"""
+User table with nick
+Args: AbstractUser: username,first_name,last_name,email,password,groups,user_permissions,is_staff,is_active,is_superuser,last_login,date_joined
+"""
 class User(AbstractUser):
         nick = models.CharField(max_length=20)
 
-#Review Table with user,concert,content,photo,sit_sector,stars,up_votes,down_votes,date
+"""
+Review Table with user,concert,content,photo,sit_sector,stars,up_votes,down_votes,date
+Args: models.Model
+"""
 class Review(models.Model):
         user = models.ForeignKey('User', on_delete=models.CASCADE)
         concert = models.ForeignKey('Concert', on_delete=models.CASCADE) #opcional
@@ -19,34 +23,50 @@ class Review(models.Model):
         down_votes = models.PositiveSmallIntegerField()
         date = models.DateTimeField()
 
-#Comment table with user,content,review,date
+"""
+Comment table with user,content,review,date 
+Args: models.Model
+"""
 class Comment(models.Model):
         user = models.ForeignKey('User', on_delete=models.CASCADE)
         content = models.TextField()                #models.CharField(max_length=500)
         review = models.ForeignKey('Review', on_delete=models.CASCADE)
         date = models.DateTimeField()
-        
-#Concert table with title,artist,date,location
+
+
+"""
+Concert table with title,artist,date,location
+Args: models.Model
+"""
 class Concert(models.Model):
         title = models.CharField(max_length=100)
         artist = models.CharField(max_length=100)
         date = models.DateField()
         location = models.ForeignKey('Location', on_delete=models.CASCADE)
 
-#Location table with name,addres,city,country
+"""
+Location table with name,addres,city,country
+Args: models.Model
+"""
 class Location(models.Model):
         name = models.CharField(max_length=100)
         address = models.CharField(max_length=200)
         city = models.CharField(max_length=100)
         country = models.CharField(max_length=100)
 
+"""
 #Vote_Comment table with user,comment,is_positive
+Args: models.Model
+"""
 class Vote_Comment(models.Model):
         user = models.ForeignKey('User', on_delete=models.CASCADE)
         comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
         is_positive = models.BooleanField()
 
+"""
 #Cote_Review table with user,review,is_positive
+Args: models.Model
+"""
 class Vote_Review(models.Model):
         user = models.ForeignKey('User', on_delete=models.CASCADE)
         review = models.ForeignKey('Review', on_delete=models.CASCADE)
