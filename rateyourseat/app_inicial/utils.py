436bf876f7db86ab9c03c2ae2a40b2fa6ec5815d
Katia <katia.fredes.a@gmail.com>
2023-07-03 15:37:34 -0400

    refactor: venues in a dictionary (utils)

--- rateyourseat/app_inicial/utils.py
+++ rateyourseat/app_inicial/utils.py
@@ -1,52 +1,49 @@
 from app_inicial.models import Location
 
+initial_locations = {
+        'Club Hípico': {
+                'address': 'Club Hípico 1001',
+                'city': 'Santiago',
+                'country': 'Chile',
+        },
+        'Teatro Cariola': {
+                'address': 'San Diego 246',
+                'city': 'Santiago',
+                'country': 'Chile',
+        },
+        'Teatro Caupolicán': {
+                'address': 'San Diego 850',
+                'city': 'Santiago',
+                'country': 'Chile',
+        },
+        'Movistar Arena': {
+                'address': 'Tupper 250',
+                'city': 'Santiago',
+                'country': 'Chile',
+        },
+        'Estadio Nacional': {
+                'address': 'Av. Grecia 2001',
+                'city': 'Santiago',
+                'country': 'Chile',
+        },
+        'Estadio Monumental': {
+                'address': 'Av. Marathon 5300',
+                'city': 'Santiago',
+                'country': 'Chile',
+        },
+        'Estadio Bicentenario de la Florida': {
+                'address': 'Av. Vicuña Mackenna 1175',
+                'city': 'Santiago',
+                'country': 'Chile',
+        },
+}
+
 def create_initial_locations():
-        if Location.objects.filter(name='Club Hípico').exists()!=True:
-                Location.objects.create(
-                        name='Club Hípico', 
-                        address='Club Hípico 1001',
-                        city='Santiago',
-                        country='Chile',
-                ).save()
-        if Location.objects.filter(name='Teatro Cariola').exists()!=True:
-                Location.objects.create(
-                        name='Teatro Cariola',
-                        address='San Diego 246',
-                        city='Santiago',
-                        country='Chile',
-                ).save()
-        if Location.objects.filter(name='Teatro Caupolicán').exists()!=True:
-                Location.objects.create(
-                        name='Teatro Caupolicán',
-                        address='San Diego 850',
-                        city='Santiago',
-                        country='Chile',
-                ).save()
-        if Location.objects.filter(name='Movistar Arena').exists()!=True:
-                Location.objects.create(
-                        name='Movistar Arena',
-                        address='Tupper 250',
-                        city='Santiago',
-                        country='Chile',
-                ).save()
-        if Location.objects.filter(name='Estadio Nacional').exists()!=True:
-                Location.objects.create(
-                        name='Estadio Nacional',
-                        address='Av. Grecia 2001',
-                        city='Santiago',
-                        country='Chile',
-                ).save()
-        if Location.objects.filter(name='Estadio Monumental').exists()!=True:
-                Location.objects.create(
-                        name='Estadio Monumental',
-                        address='Av. Marathon 5300',
-                        city='Santiago',
-                        country='Chile',
-                ).save()
-        if Location.objects.filter(name='Estadio Bicentenario de la Florida').exists()!=True:
-                Location.objects.create(
-                        name='Estadio Bicentenario de la Florida',
-                        address='Av. Vicuña Mackenna 1175',
-                        city='Santiago',
-                        country='Chile',
-                ).save()
+        for location in initial_locations:
+                if Location.objects.filter(name=location).exists()!=True:
+                        Location.objects.create(
+                                name=location,
+                                address=initial_locations[location]['address'],
+                                city=initial_locations[location]['city'],
+                                country=initial_locations[location]['country'],
+                        ).save()
